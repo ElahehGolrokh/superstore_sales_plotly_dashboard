68428b7fe6dcdf24185b518f6b5cfc1a9dcd86c4
Eli69Pro <elaheh.golrokh@gmail.com>
2024-10-31 13:19:03 +0330

    overall layout

--- src/dashboard.py
+++ src/dashboard.py
@@ -1,3 +1,4 @@
+import dash_mantine_components as dmc
 import plotly.express as px
 import pandas as pd
 
@@ -12,17 +13,30 @@ def get_app(df: pd.DataFrame) -> Dash:
     app = Dash()
 
     # App layout
-    app.layout = [
+    app.layout = html.Div([
         html.Div(html.H1(children='Superstore Sales Analysis', style={'textAlign': 'center'}),
-                 style={'background-color': '#133E87',
-                        'color': '#F3F3E0',
-                        'padding': 10,
-                        'border-radius': 8}),
+                    style={'background-color': '#133E87',
+                            'color': '#F3F3E0',
+                            'padding': 10,
+                            'border-radius': 8,
+                            'margin-bottom': 5}),
+        dmc.Grid([
+            dmc.Col([
+                html.Div(['This is the first column text'], style={'background-color': '#133E87',
+                                                                   'height': '100vh',
+                                                                   'border-radius': 8})
+            ], span=3),
+
+            dmc.Col([
+                html.Div(['This is the second column text'], style={'background-color': '#F3F3E0',
+                                                                    'height': '100vh',
+                                                                    'border-radius': 8})
+            ], span='auto'),
+        ]),
         create_dropdown(df['Category'].unique(),
                         'Select category',
                         'category'),
-        create_barchart('category')
-    ]
+        create_barchart('category')])
     return app
 
 
